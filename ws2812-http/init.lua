8550d6463e22e815e5c9370ffb80a98584a04f34
Christian Unhold <c.unhold@gmail.com>
2016-03-22 18:25:35 +0100

    GPIO15=pin8 to input to avoid current over pull-down. It was output high by default.

--- ws2812-http/init.lua
+++ ws2812-http/init.lua
@@ -1,3 +1,4 @@
+gpio.mode(8,gpio.INPUT)
 tmr.alarm(0,5000,tmr.ALARM_SINGLE,function() dofile("user.lua") end)
-uart.on("data",0,function()	tmr.stop(0) end,1)
+uart.on("data",0,function() tmr.stop(0) end,1)
 print("Enter anything to stop boot")
--- ws2812-http/user.lua
+++ ws2812-http/user.lua
@@ -1,3 +1,4 @@
+gpio.mode(8,gpio.INPUT)
 dofile("rgb.lua")
 rgb_init()
 rgb_setup(rgb_srv)
